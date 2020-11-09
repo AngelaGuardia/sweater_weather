24a4182a0420a15a0453cdf2fd4cf6d28227dfd1
Angela Guardia <angelaguardia20@gmail.com>
2020-11-09 15:57:25 -0800

    Add MapFacade Spec

--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 describe MapFacade do
-  xit "does a thing" do
+  it "returns lat long for a location" do
+    params = { location: 'denver,co' }
+    lat_lng = MapFacade.lat_lng(params)
+
+    expect(lat_lng).to be_a(Hash)
+    expect(lat_lng).to have_key(:lat)
+    expect(lat_lng[:lat]).to eq(39.738453)
+    expect(lat_lng).to have_key(:lng)
+    expect(lat_lng[:lng]).to eq(-104.984853)
+  end
+
+  xit "refactor with vcr" do
 
   end
 end
