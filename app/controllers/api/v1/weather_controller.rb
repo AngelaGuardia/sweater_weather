cb8612ebe4a68c1080bf2c8570e700a90cecbaf0
Angela Guardia <angelaguardia20@gmail.com>
2020-11-08 17:34:50 -0800

    Create weather controller

---
+++ app/controllers/api/v1/weather_controller.rb
@@ -0,0 +1,28 @@
+class Api::V1::WeatherController < ApplicationController
+  def index
+    conn = Faraday.new('http://www.mapquestapi.com')
+
+    response = conn.get('/geocoding/v1/address') do |req|
+      req.params[:key] = ENV['MAPQUEST_API_KEY']
+      req.params[:location] = params[:location]
+      req.params[:maxResults] = 1
+    end
+
+    parsed_json = JSON.parse(response.body, symbolize_names: true)
+    lat_lng = parsed_json[:results].first[:locations].first[:latLng]
+
+    conn = Faraday.new('https://api.openweathermap.org')
+
+    response = conn.get('/data/2.5/onecall') do |req|
+      req.params[:appid] = ENV['OPEN_WEATHER_API_KEY']
+      req.params[:lat] = lat_lng[:lat]
+      req.params[:lon] = lat_lng[:lng]
+      req.params[:exclude] = 'minutely,alerts'
+      req.params[:units] = 'imperial'
+    end
+
+    parsed_json = JSON.parse(response.body, symbolize_names: true)
+
+    render json: ForecastSerializer.new(Forecast.new(parsed_json))
+  end
+end
