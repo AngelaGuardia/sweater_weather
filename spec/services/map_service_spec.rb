10f94004dc2dd7c535f38a5eb63c15d9523617bf
Angela Guardia <angelaguardia20@gmail.com>
2020-11-09 15:57:14 -0800

    Add MapService Spec

--- spec/services/map_service_spec.rb
+++ spec/services/map_service_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 describe MapService do
-  xit "does the thing" do
+  it "returns lat long for a location" do
+    params = { location: 'denver,co' }
+    lat_lng = MapService.lat_lng(params)
+
+    expect(lat_lng).to be_a(Hash)
+    expect(lat_lng).to have_key(:lat)
+    expect(lat_lng[:lat]).to eq(39.738453)
+    expect(lat_lng).to have_key(:lng)
+    expect(lat_lng[:lng]).to eq(-104.984853)
+  end
+
+  xit "refactor with vcr" do
 
   end
 end
