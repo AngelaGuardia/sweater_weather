836605df91d451fcafa74d4f7aada238ee1dd664
Angela Guardia <angelaguardia20@gmail.com>
2020-11-09 09:21:42 -0800

    WIP - Create Trail PORO

---
+++ spec/poros/trail_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe Trail do
+  it "exists and has attributes" do
+    data = File.read('./spec/fixtures/trails.json')
+
+    trails_info = JSON.parse(data, symbolize_names: true)
+
+    trail_info = trails_info[:trails].first
+
+    trail = Trail.new(trail_info)
+
+    expect(trail).to be_a(Trail)
+
+    expect(trail.name).to be_a(String)
+    expect(trail.name).to eq( "Boulder Skyline Traverse")
+    expect(trail.summary).to be_a(String)
+    expect(trail.difficulty).to eq("The classic long mountain route in Boulder.")
+    expect(trail.location).to be_a(String)
+    expect(trail.location).to eq("Superior, Colorado")
+    expect(trail.distance_to_trail).to be_a(Float)
+    expect(trail.distance_to_trail).to eq(1.0)
+  end
+end
