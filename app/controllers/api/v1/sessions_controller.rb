ab6553654a652857294767edbc3e0e02d9780629
Angela Guardia <angelaguardia20@gmail.com>
2020-11-10 12:53:51 -0800

    Create Sessions Request

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,12 @@
+class Api::V1::SessionsController < ApplicationController
+  def create
+    user_info = JSON.parse(request.raw_post, symbolize_names: true)
+    user = User.find_by(email: user_info[:email])
+    if user && user.authenticate(user_info[:password])
+      session[:user_id] = user.id
+      render json: UserSerializer.new(user)
+    else
+      render json: { errors: 'Bad credentials' }, status: :bad_request
+    end
+  end
+end
---
+++ spec/requests/api/v1/sessions_request_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe 'Sessions request' do
+  before :each do
+    payload =  {
+      "email": "whatever@example.com",
+      "password": "password",
+      "password_confirmation": "password"
+    }
+
+    post '/api/v1/users', params: payload.to_json
+  end
+
+  it "returns user info when correct credentials supplied" do
+    payload =  {
+              "email": "whatever@example.com",
+              "password": "password",
+            }
+
+    post '/api/v1/sessions', params: payload.to_json
+
+    expect(response.status).to eq(200)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    user_serializer_structure_check(parsed)
+    expect(parsed[:data][:attributes][:email]).to eq("whatever@example.com")
+  end
+
+  it "returns an error if bad credentials are given" do
+    payload =  {
+              "email": "whatever@example.com",
+              "password": "wrong password",
+            }
+
+    post '/api/v1/sessions', params: payload.to_json
+
+    expect(response.status).to eq(400)
+
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:errors]).to eq("Bad credentials")
+  end
+end
