aa97fb38dcb8cbbe2b19c748c56c97654ec2fd9c
Angela Guardia <angelaguardia20@gmail.com>
2020-11-10 15:24:16 -0800

    Add travel_info method to MapFacade

--- app/facades/map_facade.rb
+++ app/facades/map_facade.rb
@@ -2,4 +2,11 @@ class MapFacade
   def self.lat_lng(params)
     MapService.lat_lng(params)
   end
+
+  def self.travel_info(roadtrip_info)
+    MapService.travel_info(roadtrip_info)
+  end
 end
+# QUESTION: Is this facade requires or could I have called the service directly from the controller?
+
+# QUESTION: Are facades only required when they will be creating POROs/Accessing the DB?
--- spec/facades/map_facade_spec.rb
+++ spec/facades/map_facade_spec.rb
@@ -12,6 +12,26 @@ describe MapFacade do
     expect(lat_lng[:lng]).to eq(-104.984853)
   end
 
+  it "returns formatted_time and rounded hours" do
+    roadtrip_info = {
+                      origin: "Denver,CO",
+                      destination: "Pueblo,CO",
+                      api_key: "jgn983hy48thw9begh98h4539h4"
+                    }
+
+    time = MapService.travel_info(roadtrip_info)
+
+    expect(time).to be_a(Hash)
+
+    expect(time).to have_key(:formatted_time)
+    expect(time[:formatted_time]).to be_a(String)
+    expect(time[:formatted_time]).to eq("01:43")
+
+    expect(time).to have_key(:hours)
+    expect(time[:hours]).to be_a(Integer)
+    expect(time[:hours]).to eq(2)
+  end
+
   xit "refactor with vcr" do
 
   end
