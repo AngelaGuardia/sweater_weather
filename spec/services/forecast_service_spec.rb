1e32bff47cd79c764fb3d7cf8992798db0bd781e
Angela Guardia <angelaguardia20@gmail.com>
2020-11-09 16:12:12 -0800

    Create ForecastService

---
+++ spec/services/forecast_service_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+describe ForecastService do
+  it "returns forecast for given latitude and longitude" do
+    params = { lat: 39.738453, lng: -104.984853 }
+
+    forecast = ForecastService.forecast(params)
+
+    expect(forecast).to be_a(Hash)
+
+    expect(forecast).to have_key(:lat)
+    expect(forecast[:lat]).to be_a(Float)
+
+    expect(forecast).to have_key(:lon)
+    expect(forecast[:lon]).to be_a(Float)
+
+    expect(forecast).to have_key(:current)
+    expect(forecast[:current]).to be_a(Hash)
+
+    expect(forecast).to have_key(:hourly)
+    expect(forecast[:hourly]).to be_a(Array)
+
+    expect(forecast).to have_key(:daily)
+    expect(forecast[:daily]).to be_a(Array)
+  end
+
+  xit "refactor with vcr" do
+
+  end
+end
