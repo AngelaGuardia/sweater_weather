3642748f16cd8bcf8a3ec5ed1b4d0a9637c7aac2
Angela Guardia <angelaguardia20@gmail.com>
2020-11-10 12:39:37 -0800

    Refactor with helpers module method

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -13,25 +13,7 @@ describe 'User request' do
     expect(response.status).to eq(201)
     parsed = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed).to have_key(:data)
-    expect(parsed[:data]).to be_a(Hash)
-
-    expect(parsed[:data]).to have_key(:type)
-    expect(parsed[:data][:type]).to be_a(String)
-    expect(parsed[:data][:type]).to eq('user')
-
-    expect(parsed[:data]).to have_key(:id)
-    expect(parsed[:data][:id]).to be_a(String)
-
-    expect(parsed[:data]).to have_key(:attributes)
-    expect(parsed[:data][:attributes]).to be_a(Hash)
-
-    expect(parsed[:data][:attributes]).to have_key(:email)
-    expect(parsed[:data][:attributes][:email]).to be_a(String)
-    expect(parsed[:data][:attributes][:email]).to eq("whatever@example.com")
-
-    expect(parsed[:data][:attributes]).to have_key(:api_key)
-    expect(parsed[:data][:attributes][:api_key]).to be_a(String)
+    user_serializer_structure_check(parsed)
 
     user = User.last
 
@@ -66,7 +48,7 @@ describe 'User request' do
 
     expect(response.status).to eq(400)
     parsed = JSON.parse(response.body, symbolize_names: true)
-    
+
     expect(parsed[:errors].include? "Password confirmation doesn't match Password").to be_truthy
   end
 end
