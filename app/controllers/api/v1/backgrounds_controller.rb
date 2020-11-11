1c6008885f5311d090229a7ed0fa6c062c30ae79
Angela Guardia <angelaguardia20@gmail.com>
2020-11-10 19:47:22 -0800

    Refactor BackgroundsController

--- app/controllers/api/v1/backgrounds_controller.rb
+++ app/controllers/api/v1/backgrounds_controller.rb
@@ -1,16 +1,5 @@
 class Api::V1::BackgroundsController < ApplicationController
   def index
-    conn = Faraday.new('https://api.pexels.com')
-
-    response = conn.get('/v1/search') do |req|
-      req.headers[:authorization] = ENV['PEXELS_API_KEY']
-      req.params[:query] = params[:location]
-    end
-
-    parsed_json = JSON.parse(response.body, symbolize_names: true)
-
-    image_data = parsed_json[:photos].first
-
-    render json: ImageSerializer.new(Image.new(image_data, params))
+    render json: ImageSerializer.new(ImageFacade.new_image(params))
   end
 end
