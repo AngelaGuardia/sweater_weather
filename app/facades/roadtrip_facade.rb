7840b64711358fe624538dc4165c375c6ee73ec0
Angela Guardia <angelaguardia20@gmail.com>
2020-11-10 17:44:31 -0800

    Refactor RoadtripFacade for impossible POROs

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -2,7 +2,7 @@ class RoadtripFacade
   def self.trip(roadtrip_info)
     lat_lng = MapFacade.lat_lng({location: roadtrip_info[:destination]})
     travel_info = MapFacade.travel_info(roadtrip_info)
-    forecast = ForecastFacade.forecast(lat_lng)
+    forecast = ForecastFacade.forecast(lat_lng) if travel_info[:hours]
 
     roadtrip = Roadtrip.new({ roadtrip: roadtrip_info, travel: travel_info, forecast: forecast})
   end
