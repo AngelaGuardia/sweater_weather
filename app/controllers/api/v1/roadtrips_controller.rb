cd85c03bcfc886e03a88e847cbd86ac34f6a17e0
Angela Guardia <angelaguardia20@gmail.com>
2020-11-10 17:07:30 -0800

    Refactor RoadtripsController with facade

--- app/controllers/api/v1/roadtrips_controller.rb
+++ app/controllers/api/v1/roadtrips_controller.rb
@@ -1,12 +1,6 @@
 class Api::V1::RoadtripsController < ApplicationController
   def create
     roadtrip_info = JSON.parse(request.raw_post, symbolize_names: true)
-    lat_lng = MapFacade.lat_lng({location: roadtrip_info[:destination]})
-    travel_info = MapFacade.travel_info(roadtrip_info)
-    forecast = ForecastFacade.forecast(lat_lng)
-
-    roadtrip = Roadtrip.new({ roadtrip: roadtrip_info, travel: travel_info, forecast: forecast})
-
-    render json: RoadtripSerializer.new(roadtrip)
+    render json: RoadtripSerializer.new(RoadtripFacade.trip(roadtrip_info))
   end
 end
