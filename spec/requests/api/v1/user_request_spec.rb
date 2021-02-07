df3fe21b31b6394e567184e723376afcedf79a3c
Angela Guardia <angelaguardia20@gmail.com>
2021-02-06 16:39:57 -0800

    Improve user tests

--- spec/requests/api/v1/user_request_spec.rb
+++ spec/requests/api/v1/user_request_spec.rb
@@ -32,7 +32,7 @@ describe 'User request' do
     expect(response.status).to eq(400)
     parsed = JSON.parse(response.body, symbolize_names: true)
 
-    expect(parsed[:errors].include? "Password can't be blank").to be_truthy
+    expect(parsed[:errors].include? "Email can't be blank").to be_truthy
     expect(parsed[:errors].include? "Password can't be blank").to be_truthy
     expect(parsed[:errors].include? "Password confirmation doesn't match Password").to be_truthy
   end
@@ -50,5 +50,21 @@ describe 'User request' do
     parsed = JSON.parse(response.body, symbolize_names: true)
 
     expect(parsed[:errors].include? "Password confirmation doesn't match Password").to be_truthy
+  end
+
+  it "return an error message if email is not unique" do
+    payload =  {
+              "email": "whatever@example.com",
+              "password": "password",
+              "password_confirmation": "password"
+            }
+
+    post '/api/v1/users', params: payload.to_json
+    post '/api/v1/users', params: payload.to_json
+
+    expect(response.status).to eq(400)
+    parsed = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed[:errors].include? "Email has already been taken").to be_truthy
   end
 end
