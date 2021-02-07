acabe640e971f8900865ec0146bacf0b85fad3ab
Angela Guardia <angelaguardia20@gmail.com>
2021-02-06 16:03:28 -0800

    Refactor forecast intialize method

--- app/poros/forecast.rb
+++ app/poros/forecast.rb
@@ -6,16 +6,8 @@ class Forecast
 
   def initialize(data)
     @current_weather = CurrentWeather.new(data[:current])
-    @daily_weather = set_daily(data[:daily].first(5))
-    @hourly_48_hours = set_hourly(data[:hourly])
+    @daily_weather = data[:daily][0..4].map { |day| DailyWeather.new(day) }
+    @hourly_48_hours = data[:hourly].map { |hour| HourlyWeather.new(hour) }
     @hourly_weather = @hourly_48_hours.first(8)
-  end
-
-  def set_daily(days)
-    days.map { |day| DailyWeather.new(day) }
-  end
-
-  def set_hourly(hours)
-    hours.map { |hour|  HourlyWeather.new(hour) }
   end
 end
