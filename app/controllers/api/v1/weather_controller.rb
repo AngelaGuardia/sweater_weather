4027bf74d537a9a76601339c62cb20ecc1ab7da3
Angela Guardia <angelaguardia20@gmail.com>
2021-02-08 17:52:40 -0800

    Make units and optional parameter for forecast

--- app/controllers/api/v1/weather_controller.rb
+++ app/controllers/api/v1/weather_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::WeatherController < ApplicationController
   def index
     lat_lng = MapFacade.lat_lng(params)
     if lat_lng
-      render json: ForecastSerializer.new(ForecastFacade.forecast(lat_lng))
+      render json: ForecastSerializer.new(ForecastFacade.forecast({lat_lng: lat_lng, units: params[:units]}))
     else
       render json: { errors: 'Location not found' }.to_json, status: :bad_request
     end
