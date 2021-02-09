920e42d8c9a4ab7d6aaec73374b3bec2c8d00779
Angela Guardia <angelaguardia20@gmail.com>
2021-02-08 18:48:11 -0800

    Refactor roadtrip initialize method

--- app/poros/roadtrip.rb
+++ app/poros/roadtrip.rb
@@ -8,15 +8,18 @@ class Roadtrip
     @start_city = info[:roadtrip][:origin]
     @end_city = info[:roadtrip][:destination]
     @travel_time = info[:travel][:formatted_time]
+    @weather_at_eta = set_weather(info)
+  end
 
+  def set_weather(info)
     if info[:travel][:hours]
       hourly_forecast = info[:forecast].hourly_48_hours[info[:travel][:hours] - 1]
-      @weather_at_eta = {
-                          temperature: hourly_forecast.predicted_temp,
-                          conditions: hourly_forecast.conditions
+      {
+        temperature: hourly_forecast.predicted_temp,
+        conditions: hourly_forecast.conditions
       }
     else
-      @weather_at_eta = {}
+      {}
     end
   end
 end
--- spec/poros/roadtrip_spec.rb
+++ spec/poros/roadtrip_spec.rb
@@ -30,4 +30,32 @@ describe Roadtrip do
       expect(roadtrip.weather_at_eta[:conditions]).to eq("few clouds")
     end
   end
+
+  it "can set the weather" do
+    VCR.use_cassette('denver_pueblo_roadtrip') do
+      roadtrip_info = {
+                        origin: "Denver,CO",
+                        destination: "Pueblo,CO",
+                        api_key: "jgn983hy48thw9begh98h4539h4"
+                      }
+
+      travel_info = {
+                      formatted_time: "01:43",
+                      hours: 2
+                    }
+
+      lat_lng = { lat: 38.265425, lng: -104.610415 }
+
+      forecast = ForecastFacade.forecast({lat_lng: lat_lng})
+
+      roadtrip = Roadtrip.new({ roadtrip: roadtrip_info, travel: travel_info, forecast: forecast})
+
+      weather_at_eta = roadtrip.set_weather({ roadtrip: roadtrip_info, travel: travel_info, forecast: forecast})
+
+      expect(weather_at_eta).to be_a(Hash)
+
+      expect(weather_at_eta[:temperature]).to eq(39.02)
+      expect(weather_at_eta[:conditions]).to eq("few clouds")
+    end
+  end
 end
