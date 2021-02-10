452a79897f83eb52e8467da4417c9b73d5ba3049
Angela Guardia <angelaguardia20@gmail.com>
2021-02-09 17:14:11 -0800

    Call MapService instead of MapFacade in Roadtrip Facade

--- app/facades/roadtrip_facade.rb
+++ app/facades/roadtrip_facade.rb
@@ -1,7 +1,7 @@
 class RoadtripFacade
   def self.trip(roadtrip_info)
-    lat_lng = MapFacade.lat_lng({location: roadtrip_info[:destination]})
-    travel_info = MapFacade.travel_info(roadtrip_info)
+    lat_lng = MapService.lat_lng({location: roadtrip_info[:destination]})
+    travel_info = MapService.travel_info(roadtrip_info)
     forecast = ForecastFacade.forecast({lat_lng: lat_lng, units: roadtrip_info[:units]}) if travel_info[:hours]
 
     Roadtrip.new({ roadtrip: roadtrip_info, travel: travel_info, forecast: forecast})
